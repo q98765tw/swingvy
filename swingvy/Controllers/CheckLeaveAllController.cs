4a3b09b24f84a1422ff271dca5b04d6f0b790890
q98765tw <q98765tw@gmail.com>
2023-09-13 17:27:16 +0800

    first commit

---
+++ swingvy/Controllers/CheckLeaveAllController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace swingvy.Controllers
+{
+    public class CheckLeaveAllController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ swingvy/Controllers/CheckLeavePersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace swingvy.Controllers
+{
+    public class CheckLeavePersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
