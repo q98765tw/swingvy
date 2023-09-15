bc0038445753b9a0544406a57781879f71d1e6a5
q98765tw <q98765tw@gmail.com>
2023-09-15 16:32:57 +0800

    審核畫面顯示

--- swingvy/Controllers/CheckLeaveAllController.cs
+++ swingvy/Controllers/CheckLeaveAllController.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using swingvy.Models;
 
 namespace swingvy.Controllers
 {
     public class CheckLeaveAllController : Controller
     {
+        private readonly swingvyContext _swingvyContext;
+        public CheckLeaveAllController(swingvyContext context)
+        {
+            _swingvyContext = context;
+        }
         public IActionResult Index()
         {
-            string? member_id = Request.Cookies["member_id"];
-            string? member_head = Request.Cookies["member_head"];
-            if (member_id != member_head) { return RedirectToAction("Privacy", "Home"); }
+            string? member_position = Request.Cookies["member_position"];
+            if (member_position == "0") { return RedirectToAction("Privacy", "Home"); }
+            string? member_type = Request.Cookies["member_type"];
+            int.TryParse(member_type, out int memberType);
+            var query = from leaveOrder in _swingvyContext.leaveOrder
+                        join md1 in _swingvyContext.memberData on leaveOrder.member_id equals md1.member_id
+                        where md1.type == memberType
+                        select new
+                        {
+                            name = md1.name,
+                            applyTime = leaveOrder.applyTime,
+                            leaveType = leaveOrder.type,
+                            startTime = leaveOrder.startTime,
+                            endTime = leaveOrder.endTime,
+                            state = leaveOrder.state
+                        };
+            var resultList = query.ToList();
+            ViewBag.leaveList = resultList;
+            ViewBag.memberType = memberType;
             return View();
         }
     }
