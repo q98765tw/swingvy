ef865cb66d7eac4d3f02415261ddab7aa58e413f
q98765tw <q98765tw@gmail.com>
2023-09-21 13:47:12 +0800

    刪除垃圾和註解

--- swingvy/Controllers/LoginController.cs
+++ swingvy/Controllers/LoginController.cs
@@ -22,19 +22,15 @@ namespace swingvy.Controllers
         [HttpPost]
         public ActionResult Login(string account, string password)
         {
-            // 帐号密码验证
             var user = _swingvyContext.member.FirstOrDefault(m => m.account == account);
             if (user != null)
             {
-                // 验证密码哈希值
+                //驗證hash
                 bool isPasswordCorrect = VerifyPasswordHash(password, user.PasswordHash, user.Salt);
 
                 if (isPasswordCorrect)
                 {
-                    // 登录成功
-                    // 根据需要执行其他登录相关的操作，例如设置认证Cookie等
-
-                    // 将用户帐号存入ViewBag以供后续使用
+                    // 登入成功
                     var id = user.member_id;
                     var data = _swingvyContext.memberData.FirstOrDefault(m => m.member_id == id);
                     if (data != null)
@@ -49,8 +45,7 @@ namespace swingvy.Controllers
                 }
             }
 
-            // 登录失败
-            // 可以执行相应的失败处理逻辑，例如显示错误消息
+            // 登入失败
             ViewBag.ErrorMessage = "帐号或密码错误";
             ModelState.Clear();
             return View("Index");
--- swingvy/Controllers/RegisterController.cs
+++ swingvy/Controllers/RegisterController.cs
@@ -23,12 +23,7 @@ namespace swingvy.Controllers
         [HttpPost]
         public ActionResult Register(int type,int position, string account, string password)
         {
-            if (type == null || position == null || account == null || password == null) {
-                ViewBag.ErrorMessage = "用户名已存在";
-                ModelState.Clear();
-                return View("Index");
-            }
-            // 检查用户名是否已存在
+            // 檢查用户名是否已存在
             var existingUser = _swingvyContext.member.FirstOrDefault(m => m.account == account);
             if (existingUser != null)
             {
@@ -53,9 +48,8 @@ namespace swingvy.Controllers
             };
             _swingvyContext.member.Add(newUser);
             _swingvyContext.SaveChanges();
-            // 注册成功，可以执行其他操作，如登录用户等
+            //判斷主管員工，並自動登入
             var user = _swingvyContext.member.FirstOrDefault(m => m.account == account && m.password == password);
-            
             if (user != null)
             {
                 if (position == 1)
